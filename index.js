35e6fca8d281045f641cf5cdd26bafd12e384d1b
suhaybzz <suhaybahmed07@gmail.com>
2025-10-14 16:06:54 +0100

    vm test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ http.createServer(function(req, res) {
       <title>My Page</title>
     </head>
     <body>
-      <h1>Hello, I'm Suhayb Ahmed!</h1>
+      <h1>Hello, I'm Suhayb Ahmed! and I study at Goldsmiths.</h1>
       <h2>Student ID: 33742447</h2>
       <p>I am learning how to build web apps with Node.js...</p>
     </body> 
